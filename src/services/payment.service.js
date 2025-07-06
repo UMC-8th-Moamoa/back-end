258e99cfd74534c314536c7127d3108f3e70cbcc
rudals02 <lkmm7460@gmail.com>
2025-07-06 19:33:44 +0900

    GET, POST 주석

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,1 +1,97 @@
 //결제 API 연동, 모금액 분배 로직
+
+/**
+ * @swagger
+ * tags:
+ *   name: Payments
+ *   description: 결제 관리 API
+ */
+
+/**
+ * @swagger
+ * /api/payments:
+ *   get:
+ *     summary: 모든 결제 목록 조회
+ *     tags: [Payments]
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                       amount:
+ *                         type: number
+ *                       status:
+ *                         type: string
+ */
+
+/**
+ * @swagger
+ * /api/payments:
+ *   post:
+ *     summary: 새 결제 생성
+ *     tags: [Payments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - groupId
+ *               - amount
+ *             properties:
+ *               groupId:
+ *                 type: string
+ *                 description: 그룹 ID
+ *               amount:
+ *                 type: number
+ *                 description: 결제 금액
+ *               paymentMethod:
+ *                 type: string
+ *                 description: 결제 방법
+ *                 enum: [card, bank_transfer, kakao_pay]
+ *     responses:
+ *       201:
+ *         description: 결제 생성 성공
+ *       400:
+ *         description: 잘못된 요청
+ */
+
+/**
+ * @swagger
+ * /api/payments/{id}:
+ *   get:
+ *     summary: 특정 결제 조회
+ *     tags: [Payments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: 결제 ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 성공
+ *       404:
+ *         description: 결제를 찾을 수 없음
+ */
+
+
+class PaymentService {
+  // TODO: 구현 예정
+}
+
+module.exports = PaymentService;//결제 API 연동, 모금액 분배 로직
